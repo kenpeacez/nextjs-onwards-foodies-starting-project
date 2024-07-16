4659060e52d932086cf4ff44793f05e5a434985b
kenpeacez <kenpeacezx@gmail.com>
2024-07-16 13:56:11 +0800

    No. 107, 108, 109 - Added codebase to output images - Set up the database capabilities using SQLite by using 'npm i better-sqlite3' - Fetching data from the database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,14 +1,27 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
-      <main>
-        <h1>Meal Page</h1>
-        <p>Welcome to our meal page!</p>
-        <Link href="meals/post-1">Post 1</Link>
-        <p></p>
-        <Link href="meals/post-2">Post 2</Link>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It's easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals}></MealsGrid>
       </main>
     </>
   );
